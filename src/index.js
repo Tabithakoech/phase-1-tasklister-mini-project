273fb7417bf0049a3eb205fd0ee94a138ba2b1a9
TabithaKoech <tabbyk1718@gmail.com>
2022-06-13 19:13:58 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const submit= document.querySelector("#form");
+
+  submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  todo()
+  });
+})
+
+function todo(){
+  const input = document.getElementById("new-task-description").value;
+  const newTodo= document.createElement("li");
+  const buttonCreate = document.createElement("button");
+
+  newTodo=${input.value};
+  document.getElementById("tasks").appendChild(newTodo);
+
+}
